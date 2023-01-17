0d0f4adeb15fc7cc4c4159e3345a763ca3258744
Gianluca Tegano <gtegano@e3r12p7.42roma.it>
2023-01-17 13:01:40 +0100

    save01

---
+++ libft/ft_memset.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtegano <gtegano@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 13:00:11 by gtegano           #+#    #+#             */
+/*   Updated: 2023/01/17 13:00:29 by gtegano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	if (n)
+		while (n--)
+			*str++ = (unsigned char)c;
+	return (s);
+}
